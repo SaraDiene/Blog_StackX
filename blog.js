e477af7832bdd8c609ac791212e5a93363be15c3
Sara Abreu <webpro.sara@gmail.com>
2023-10-04 14:08:07 -0300

    Criação do projeto para entrega da tarefa.

---
+++ blog.js
@@ -0,0 +1,405 @@
+// Dados simulados de postagens
+const posts = [
+    {
+        id: 1,
+        title: `Aqui estão as IAs que irão poupar horas de trabalho`,
+        content:`
+
+        Sabe aquele instinto de que você trabalha mais atualmente do que no 
+        passado? Aquela sensação de que você é incapaz de poupar tempo para as atividades 
+        pessoais?
+        <br><br>
+
+        Você está correto! Uma pesquisa da Nielsen em parceria com a Toluna apontou que 
+        23% dos brasileiros trabalham de 10 a 15 horas a mais por semana, o que mostra uma 
+        dificuldade dos profissionais em equilibrar as atividades pessoais e profissionais 
+        no home office. Contudo, esse aumento também é um indício da ausência de 
+        autogerenciamento, algo que pode ser contornado com a utilização de ferramentas 
+        de gestão de tarefas, por exemplo.
+        <br><br>
+
+        Cada vez mais populares no ambiente de trabalho, os softwares para organização 
+        das tarefas suprem a necessidade que temos de gerenciar os múltiplos projetos de 
+        forma ágil e prática, por meio da automação, liberando espaços preciosos na agenda 
+        dos gestores, que podem se dedicar ao planejamento estratégico de campanhas e 
+        projetos, assim como focar na gestão de pessoas, oferecendo o suporte para sua 
+        equipe.
+        <br><br>
+
+        Nesse artigo, reunimos os melhores recursos para poupar tempo incorporados a um 
+        gerenciador de tarefas, da organização das prioridades à criação de relatórios 
+        personalizados. Confira:
+        <br><br>
+       
+        <img src= "eightify.svg" class="img_post1"/>
+        <br>
+        <h2 class="titulo"> Extensão resume vídeos do YouTube com IA </h2>
+        <br>
+
+        <p class="texto_post">
+        Eightify app utiliza chatbot da OpenAI para sintetizar conteúdos e gerar insights a partir 
+                dos vídeos analisados no YouTube; recurso gratuito está disponível para Google Chrome
+        <br> <br>
+        Eightify app utiliza chatbot da OpenAI para sintetizar conteúdos e gerar insights a partir 
+                dos vídeos analisados no YouTube; recurso gratuito está disponível para Google Chrome
+        <br> <br>
+        Com o Eightify app, é possível obter resumos e identificar os principais 
+                tópicos de vídeos do YouTube em poucos segundos. 
+        <br> <br>
+        A ferramenta é uma extensão gratuita, 
+                disponível para Google Chrome, e usa o ChatGPT para analisar reportagens, podcasts, entrevistas, 
+                tutoriais, análise de produtos e qualquer outro tema disponível na plataforma de streaming.
+        <br>    <br> 
+        Além disso, o aplicativo apresenta marcadores de tempo e capítulos para cada ponto sugerido, 
+                facilitando a localização dos assuntos na plataforma de streaming. 
+        O recurso economiza tempo,ajuda a organizar informações e ainda evita que o usuário assista vídeos irrelevantes.
+        </p>
+        <br>  <br>
+
+
+        <img src= "durable.png" class="img_post1"/>
+        <br>
+        <h2 class="titulo"> O construtor de sites com IA que gera um site inteiro com imagens e texto em segundos.</h2>
+        <br> <br>
+        <p>
+        Durable é uma plataforma de criação de sites com inteligência artificial para 
+                proprietários de pequenos negócios. Com a ferramenta, é possível gerar um site 
+                em apenas 30 segundos, além de automatizar o marketing e gerenciar finanças.
+        <br> <br>
+        A plataforma é especialmente útil para proprietários de empresas individuais que desejam criar uma presença online rápida e fácil.
+        <br> <br>
+        A Durable oferece opções de design personalizáveis, bem como recursos de 
+                análise de marketing e gerenciamento financeiro para ajudar os proprietários a 
+                alcançarem seus objetivos de negócios.
+        <br> <br>
+        Com a ajuda da IA, a criação de um site se torna muito mais fácil e acessível.
+        </p>
+            `,
+        date: new Date().toLocaleString(),
+        category: "Software",
+        views: 0,
+    },
+    {
+        id: 2,
+        title: "Crie arte hipnotizante de espirais de IA de fusão em segundos",
+        content:` 
+        
+        <p>Explore a incrível fusão entre IA e arte! Crie espirais, 
+        ilusões e padrões hipnotizantes que ultrapassam os limites artísticos.</p>
+        <p>Assim como Giorgio Moroder revolucionou a dance music com o sintetizador,
+        estamos liberando uma magia artística insana.</p>
+      
+        <p>
+        Etapa 1:
+        <br>
+      
+        1. Acesse FusionArt: <a href="https://www.fusionartai.com/" target="_blank">FusionArtAi</a>
+        <br> <br>
+        2. Crie uma conta gratuita (faça login com o Google) Você terá 10 créditos gratuitamente.
+   
+        <br><br>
+
+        Etapa 2:
+        <br> <br>
+    
+        1. Escolha seu padrão ou faça upload do seu (por exemplo, o logotipo da Apple em preto e branco)
+        <br><br>
+        2. Insira o seu prompt (melhor em inglês)
+       
+        <br> <br>
+       
+        3. Aguarde e baixe a sua imagem.
+        
+        <br> <br>
+        
+        <img src="fusionart.png" alt="Imagem gerada pelo site FusionArtAi">
+        
+        </p>`,
+        date: new Date().toLocaleString(),
+        category: "Software",
+        views: 0
+    },
+    {
+        id: 3,
+        title: "iPhone 15: os detalhes do lançamento no Brasil",
+        content: `
+        
+        <p>O lançamento do iPhone 15 aconteceu no último 29 de setembro.</p>
+
+        <p>O burburinho em torno da próxima geração de celulares da Apple aumentou nos últimos 
+        dias, com especial interesse pelo iPhone 15 Pro Max.</p>
+        <p>Os novos celulares da Apple já estão à venda no exterior e recém embarcou por aqui.</p>
+        <h2>Quanto vai custar o iPhone 15?</h2>
+        <span>Confira abaixo os preços do iPhone 15 no Brasil:</span>
+        <br><br>
+        * iPhone 15: a partir de R$ 7.299<br><br>
+        * iPhone 15 Plus: a partir de R$ 8.299<br><br>
+        * iPhone 15 Pro: a partir de R$ 9.299<br><br>
+        * iPhone 15 Pro Max: a partir de R$ 10.999<br><br>
+        <h2> Especificações </h2><br><br>
+        <table>
+  <tr>
+    <th>Modelo</th>
+    <th>Cor</th>
+    <th>Capacidade</th>
+    <th>Peso</th>
+
+  </tr>
+  <tr>
+    <td>iPhone 15</td>
+    <td> Preto, azul, verde, amarelo, rosa</td>
+    <td> 128 - 256 - 512 GB </td>
+    <td>  171 gramas </td>
+  </tr>
+  <tr>
+  <td>iPhone 15 Plus</td>
+  <td> Preto, azul, verde, amarelo, rosa</td>
+  <td> 128 - 256 - 512 GB </td>
+  <td> 201 gramas </td>
+  </tr>
+  <tr>
+  <td>iPhone 15 Pro</td>
+  <td> Titânio preto, titânio branco, titânio azul, titânio natural</td>
+  <td> 128 - 256 - 512 GB 1 TB</td>
+  <td> 187 gramas </td>
+  </tr>
+  <tr>
+  <td>iPhone 15 Pro Max</td>
+  <td> Titânio preto, titânio branco, titânio azul, titânio natural</td>
+  <td> 128 - 256 - 512 GB 1 TB</td>
+  <td> 221 gramas </td>
+  </tr>
+
+</table>`,
+        date: new Date().toLocaleString(),
+        category: "Gadgets",
+        views: 0
+    },
+    {
+        id: 4,
+        title: "5 dicas para uma navegação segura na internet",
+        content: `
+        <p>Segundo o estudo Digital 2022: Global Overview Report, 
+        publicado pelo site Datareportal, atualmente são quase 5 bilhões de pessoas no mundo
+        estão conectadas à internet, o que corresponde nada menos do que 63% da 
+         população mundial.</p>
+        <p>E, de acordo com a empresa de segurança Check Point Software, 
+         os ciberataques não param de crescer no Brasil: o país registrou aumento de 46% no 
+         segundo trimestre de 2022, se comparado ao mesmo período de 2021. Portanto, 
+         diminuir os riscos durante a navegação na web é algo imprescindível atualmente.</p>
+        <h2>Como navegar sem riscos na internet?</h2>
+        <p>Os especialistas da Check Point Software compartilham dez dicas essenciais que 
+        todo internauta deve saber para ter uma navegação segura e evitar que dados 
+        pessoais e corporativos sejam comprometidos:</p>
+        <p>
+        <strong>1. Não confie apenas nos navegadores para proteção contra páginas maliciosas</strong><br>
+        Os navegadores, sozinhos, não protegem você de todas as ameaças cibernéticas na 
+        internet. É por isso que se deve ter cuidado ao navegar, seja em uma plataforma de 
+        streaming ou em um e-commerce. Os cibercriminosos costumam criar sites semelhantes 
+        aos mais populares para roubar suas credenciais de acesso, detalhes de cartão de 
+        crédito ou contatos de redes sociais. Para evitar isso, é essencial verificar se a 
+        URL da página da web está correta;
+        </p>
+        <p>
+        <strong>2. Não reutilize senhas</strong><br>
+        Ter as mesmas combinações em todos os aplicativos, serviços e sites é um dos erros mais básicos e perigosos, pois basta que um cibercriminoso consiga decifrar ou roubar uma delas para poder acessar todas as outras e obter todos os dados pessoais e corporativos dos diferentes sites. O ideal é combinar oito caracteres entre letras maiúsculas, minúsculas, números e símbolos para dificultar. Se você tiver dificuldade de lembrar, pode usar um gerenciador de senhas;
+        </p>
+        <p>
+        <strong>3. Mantenha o software do navegador atualizado</strong><br>
+        Muitas vezes, alguns usuários pensam que atualizações não precisam ser efetuadas 
+        ou até mesmo deixam seus dispositivos mais lentos. Mas a realidade é que não deixar 
+        os softwares em dia aumenta muito os riscos. Para manter um computador seguro é 
+        muito importante ter programas atualizados, pois os updates traze correções de 
+        segurança;
+        </p>
+        <p>
+        <strong>4. Evite redes Wi-Fi públicas ou gratuitas</strong><br>
+        Essas redes não possuem nenhum tipo de proteção e qualquer pessoa, mesmo um 
+        cibercriminoso, pode se conectar a elas. A partir de uma rede pública sem defesas, 
+        um bandido pode acessar tudo o que está armazenado no seu dispositivo;
+        </p>
+        <p>
+        <strong>5. Ative o bloqueador de pop-ups</strong><br>
+        Muitas páginas da web e aplicativos nos bombardeiam com publicidade quando são acessados. 
+        A grande maioria esconde golpes cibernéticos, e com a simples instalação de um bloqueador 
+        você pode evitar um grande número de riscos.
+        </p>`,
+        
+        date: new Date().toLocaleString(),
+        category: "Internet",
+        views: 0
+    },
+
+    {
+        id: 5,
+        title: "Monte seu PC, como escolher peças para o seu computador novo?",
+        content: `
+        <p>Processador, placa mãe, gabinete... entenda a sopa de letrinhas dos componentes 
+        necessários para começar esse processo da máquina nova a partir do zero.</p>
+        
+        <p>Montar um PC em casa é uma tarefa que pode ser mais simples do que se imagina.    
+        Mas requer um bom planejamento prévio para comprar diversos componentes separados e o 
+        conhecimento técnico básico para encaixar as partes no local correto.</p>
+
+        <h2>As principais vantagens de montar seu próprio computador de mesa</h2>
+
+        <p>🛠 Alto nível de personalização, com gabinete e luzes RGB do jeito que quiser.</p>
+        <p>⌨️ Controle completo na escolha das partes da máquina: processador, placa mãe, memória RAM, 
+        armazenamento, gabinete, entre outras peças.</p>
+        <p>🔌 Muitos fabricantes de PCs não oferecem PCs de mesa para usuários básicos ou intermediários – 
+        só para gamers ou quem quer máquinas de alto desempenho.</p>
+        <p>🧰 Montar um também é uma alternativa de uso para trabalhar ou estudar para quem não quer ou não 
+        precisa de um notebook.</p>
+        <i><p>A principal desvantagem é a montagem em si, um quebra-cabeças com peças bem mais caras, 
+        sensíveis e sofisticadas.</p></i>
+
+        <h2>Itens essenciais para o seu computador</h2>
+        
+        <h3>Processador</h3>
+        <p>O processador é o primeiro item a levar em consideração, 
+        e a partir dele dá para começar a planejar a máquina</p>
+        <h3>Placa Mãe</h3>
+        <p>A placa mãe é a "cola" que unifica todas as partes do computador e é nela que os componentes são 
+        encaixados.</p>
+
+        <p>Sua escolha deve ser feita baseada no processador (AMD ou Intel), o máximo de memória RAM 
+        desejada e no número de conexões que o consumidor quer ter no seu PC.</p>
+        <h3>Memória RAM</h3>
+        <p>O mínimo indicado para usar em um PC é 8GB, de acordo com os especialistas.</p>
+
+        <p>Na montagem de um PC, é necessário checar se a placa mãe e a memória RAM escolhida são compatíveis com os 
+        padrões DDR4 (mais popular e barato) e DDR5 (mais cara, veloz e com a vantagem de ter maior eficiência 
+        no consumo de energia).</p>
+
+        <h3>Placa de vídeo</h3>
+        <p>A placa de vídeo, também chamada de GPU, é a parte que determina a experiência do consumidor na 
+        hora de jogar. Ela serve para aumentar o desempenho em games.</p>
+
+        <p>Pode ser uma das peças mais caras do computador, dependendo do modelo escolhido.</p>
+
+        <h3>Armazenamento</h3> 
+       <p> Os especialistas recomendam usar o SSD, disco de estado sólido, como principal local para 
+        armazenar o sistema operacional e os aplicativos.</p>
+
+       <p> O desempenho vai ser mais rápido e sem gargalos.</p>
+        
+       <p>O HD – disco rígido – é um método de armazenamento mais lento, mas com maiores capacidades. 
+       Pode ser usado em paralelo com o SSD para guardar arquivos de trabalho, fotos e vídeos, entre outros.</p>
+        
+       <h3> Fonte, Gabinete e Cooler</h3>
+       <p> Esses itens são essenciais para fornecer a estrutura do PC (gabinete), a energia para os componentes
+         (fonte) e a ventilação interna da máquina e do processador (cooler).</p>
+
+        <p>A escolha do gabinete deve ser atrelada ao tamanho da placa mãe. Os gabinetes indicados na lista
+         já vêm com a fonte de 500W e ventoinhas para ventilação.</p>
+
+        <p>A fonte requer um cálculo de consumo estimado de energia da placa mãe e da placa de vídeo, 
+        para que o PC funcione corretamente. Esse dado é fornecido pelos fabricantes.</p>
+
+       <p> Ao final do processo de montagem, os cabos internos devem ser organizados dentro do gabinete, 
+        para não atrapalhar no fluxo de ar e aquecer demais o computador.</p>
+
+        <h3>Sistema operacional</h3>
+        <p>Depois de tudo montado e organizado, é hora de ligar a máquina e instalar o sistema operacional, 
+        que pode ser o <strong>Linux </strong>– que tem versões gratuitas – ou o <strong> Windows 11 Home</strong>, 
+        da Microsoft.</p>
+
+        <h3>Monitor e periféricos</h3>
+        <p>Monitor e os periféricos – teclado, mouse, microfone externo, webcam, headset – são o último passo a 
+        escolher dessa parte de hardware do PC.</p>
+        
+`,
+        date: new Date().toLocaleString(),
+        category: "Hardware",
+        views: 0
+    },
+ 
+];
+
+// Função para exibir os cards de postagens na página inicial
+function displayPosts(categoryFilter = "") {
+    const postList = document.getElementById("post-list");
+    postList.innerHTML = "";
+
+    for (const post of posts) {
+        if (categoryFilter === "" || post.category === categoryFilter) {
+            const card = document.createElement("div");
+            card.className = "post-card";
+            card.innerHTML = `
+                <p class="date_card">${post.date}</p>
+                <p class="category_card">Categoria: ${post.category}</p>
+                <p>${getPostViews(post.id)} visualizações</p>
+                <h2 class="title_card">${post.title}</h2>
+                <p class="content_card">${post.content.substring(0, 120)}...</p>
+            `;
+            
+            const image = document.createElement("img");
+            image.src = `post${post.id}.jpeg`;
+            card.appendChild(image);
+            
+            card.addEventListener("click", () => {
+                viewPost(post);
+            });
+            postList.appendChild(card);
+        }
+    }
+}
+
+// Função para obter o número de visualizações de um post do localStorage
+function getPostViews(postId) {
+    const views = localStorage.getItem(`postViews_${postId}`);
+    return views ? parseInt(views) : 0;
+}
+
+// Função para salvar o número de visualizações de um post no localStorage
+function savePostViews(postId, views) {
+    localStorage.setItem(`postViews_${postId}`, views.toString());
+}
+
+// Função para exibir a página de visualização do post
+function viewPost(post) {
+    const postList = document.getElementById("post-list");
+    postList.innerHTML = "";
+
+    const postId = post.id;
+    const postViews = getPostViews(postId);
+
+    const postView = document.createElement("div");
+    postView.className = "post-view";
+    postView.innerHTML = `
+        <p>Data: ${post.date}</p>
+        <p>Categoria: ${post.category}</p>
+        <p>Visualizações: ${postViews + 1}</p>
+        <h1>${post.title}</h1>
+        <p>${post.content}</p>
+    `;
+
+    // Incrementa o número de visualizações e salva no localStorage
+    savePostViews(postId, postViews + 1);
+
+    postList.appendChild(postView);
+}
+
+// Adiciona os eventos de clique aos links de categorias
+document.getElementById("all").addEventListener("click", () => {
+    displayPosts("");
+});
+
+document.getElementById("hardware").addEventListener("click", () => {
+    displayPosts("Hardware");
+});
+
+document.getElementById("software").addEventListener("click", () => {
+    displayPosts("Software");
+});
+
+document.getElementById("gadgets").addEventListener("click", () => {
+    displayPosts("Gadgets");
+});
+
+document.getElementById("internet").addEventListener("click", () => {
+    displayPosts("Internet");
+});
+
+// Exibe todas as postagens na inicialização
+displayPosts("");
